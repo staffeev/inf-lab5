3864d892164477c4031de7eefabc8f480dd79b65
staffeev <st.affeev@yandex.ru>
2023-12-12 22:46:53 +0300

    new cbs file

--- check_cbs.sh
+++ check_cbs.sh
@@ -33,7 +33,7 @@ function  process_file() {
 		local result=$?
 		if [[ "$result" == 1 ]]; then
 			echo "There is incorrect bracket sequence in line" "$counter" "in file" "$1"
-			return 1
+			flag=1
 		fi
 	done < "$1"
 	return 0
@@ -44,7 +44,6 @@ IFS=""
 while read -r filename || [ -n "$filename" ]
 do	
 	process_file $filename
-	flag=$((flag+$?))
 done < <(find . -type f -name '*.txt')
 
 if [[ "$flag" > 0 ]]; then
