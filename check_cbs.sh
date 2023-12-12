2c72a2e48fb55d124249a5cfa00b9afb03f38775
staffeev <st.affeev@yandex.ru>
2023-12-12 22:35:53 +0300

    fixed

--- check_cbs.sh
+++ check_cbs.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-
-
 function process_line(){
-	count=0
-	line="$1"
+	local count=0
+	local line="$1"
 	for ((i=0; i<${#line}; i++))
 	do
-		symb=${line:i:1}
+		local symb=${line:i:1}
 		if [[ "$symb" == "(" ]]; then
 			count=$((count+1))
 		elif [[ "$symb" == ")" ]]; then
@@ -20,30 +18,33 @@ function process_line(){
 		fi
 	done 
 	if  [[ "$count" == 0 ]]; then
-		return 1
-	else
 		return 0
+	else
+		return 1
 	fi
-	
 }
 
 function  process_file() {
 	local counter=0
-	flag=0
 	while read -r line || [ -n "$line" ]
 	do
 		counter=$((counter+1))
-		if process_line $line; then
-			echo "There is incorrect bracket sequence in line" "$counter" "in file" "$1" >&2
-			flag=$((flag+1))
+		process_line $line
+		local result=$?
+		if [[ "$result" == 1 ]]; then
+			echo "There is incorrect bracket sequence in line" "$counter" "in file" "$1"
+			return 1
 		fi
 	done < "$1"
+	return 0
 }
 
+flag=0
 IFS=""
 while read -r filename || [ -n "$filename" ]
 do	
 	process_file $filename
+	flag=$((flag+$?))
 done < <(find . -type f -name '*.txt')
 
 if [[ "$flag" > 0 ]]; then
